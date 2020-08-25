2a418e2e5f4f84a9bbf6279727de32c3938edc73
inesnago <imsnago@gmail.com>
2020-08-25 10:32:02 +0000

    forgot to add js script on last commit

--- assets/js/myscripts.js
+++ assets/js/myscripts.js
@@ -5,3 +5,49 @@ $('.main-button').mouseenter(function(){
 $('.main-button').mouseleave(function(){
     $(this).removeClass('shade');
 });
+
+$('#north-button').click(function(){
+    $("#north-main").slideToggle("slow");
+    
+    if($("#west-button") || $("#south-button") == 'open'){
+        $("north-main").hide();
+    }
+});
+
+$('#west-button').click(function(){
+    $("#west-main").slideToggle("slow");
+});
+
+
+/*function toggleMainNorth(){
+
+    var toggle = document.getElementById("north-main");
+    if (toggle.style.display === "none"){
+        toggle.style.display = "inline-block";
+    } else{
+        toggle.style.display = "none";
+    }
+   
+}
+
+function toggleMainWest(){
+    
+    var toggle = document.getElementById("west-main");
+        
+    if (toggle.style.display === "none"){
+        toggle.style.display = "inline-block";
+    } else{
+        toggle.style.display = "none";
+    }
+}
+
+function toggleMainSouth(){
+    
+    var toggle = document.getElementById("south-main");
+        
+    if (toggle.style.display === "none"){
+        toggle.style.display = "inline-block";
+    } else{
+        toggle.style.display = "none";
+    }
+}*/

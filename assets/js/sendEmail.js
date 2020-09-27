718746756a28aca90e1362b2e070049367f5921d
inesnago <imsnago@gmail.com>
2020-09-27 19:19:55 +0000

    both emails working

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,2 +1,45 @@
+function sendMail(contactForm){
+    emailjs.send("gmail", "Surf The West Country", {
+        "from_name": contactForm.firstName.value,
+        "from_email": contactForm.email.value,
+        "message_html": contactForm.terms.value
+    })
 
-console.log("Hello");
+    .then(
+        function(response){
+            console.log("SUCCESS", response);
+        },
+        function(error){
+            console.log("FAILED", error);
+        }
+    );
+
+    return false;
+
+}
+
+function addSpotMail(surfForm){
+    emailjs.send("gmail", "New Surf Spot", {
+        "spot_name": surfForm.spotName.value,
+        "spot_location": surfForm.spotLocation.value,
+        "break-type": surfForm.break.value,
+        "parking": surfForm.parking.value,
+        "lifeguard": surfForm.lifeguard.value,
+        "surfschool": surfForm.surfschool.value,
+        "disabled": surfForm.disabled.value,
+        "crowd_level": surfForm.crowdLevel.value,
+        "additional": surfForm.additional.value,
+        "public": surfForm.public.value
+    })
+
+    .then(
+        function(responseTwo){
+            console.log("SUCCESS", responseTwo);
+        },
+        function(errorTwo){
+            console.log("FAILED", errorTwo);
+        }
+    );
+
+    return false;
+}
